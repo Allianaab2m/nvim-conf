60abbfc1d171eeb3b1cda5dcb3159b1b0f9b208b
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2022-12-17 14:29:59 +0900

    initial

---
+++ init.lua
@@ -0,0 +1,191 @@
+local util = require('util')
+local assign = util.assign
+
+local jetpackfile = vim.fn.stdpath('data') .. '/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim'
+local jetpackurl = 'https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim'
+if vim.fn.filereadable(jetpackfile) == 0 then
+  vim.fn.system(string.format('curl -fsSLo %s --create-dirs %s', jetpackfile, jetpackurl))
+end
+
+assign(vim.g) {
+  belloff = 'all',
+  did_install_default_menus = 1,
+  did_install_syntax_menu = 1,
+  skip_loading_mswin = 1,
+  loaded_2html_plugin = 1,
+  loaded_gzip = 1,
+  loaded_man = 1,
+  loaded_netrwPlugin = 1,
+  loaded_remote_plugins = 1,
+  loaded_spellfile_plugin = 1,
+  loaded_tarPlugin = 1,
+  loaded_tutor_mode_plugin = 1,
+  loaded_zipPlugin = 1,
+}
+
+assign(vim.opt) {
+  cmdheight = 0,
+  encoding = 'utf-8',
+  fileencodings = 'iso-2022-jp,euc-jp,sjis,utf-8',
+  fileformats = 'unix,dos,mac',
+  list = true,
+  listchars = 'tab:^  ,trail:-',
+  mouse = '',
+  number = true,
+  relativenumber = true,
+  expandtab = true,
+  tabstop = 2,
+  shiftwidth = 2,
+  scrolloff = 2,
+  termguicolors = true,
+  completeopt = 'menu,menuone,noselect',
+}
+
+require('jetpack-init') {
+  { 'tani/vim-jetpack', opt = 1 },
+
+  -- Startup time
+  { 'dstein64/vim-startuptime', cmd = 'StartupTime', opt = 1 },
+
+  -- Notification
+  { 'rcarriga/nvim-notify',
+    config = function()
+      require('notify').setup {}
+    end,
+  },
+
+  -- Scrollbar
+  { 'petertriho/nvim-scrollbar',
+    event = {
+      'BufRead',
+      'BufNewFile',
+      'BufWinEnter',
+      'CmdwinLeave',
+      'TabEnter',
+      'TermEnter',
+      'TextChanged',
+      'VimResized',
+      'WinEnter',
+      'WinScrolled',
+    },
+    config = function()
+      require('scrollbar').setup {}
+    end,
+  },
+
+  -- Smooth scroll
+  { 'psliwka/vim-smoothie',
+    event = {
+      'BufRead',
+      'BufNewFile'
+    },
+    setup = function()
+      local g = require('util').assign(vim.g)
+
+      g {
+        smoothie_experimental_mapping = false,
+        smoothie_update_interval = 16,
+        smoothie_speed_constant_factor = 16,
+        smoothie_speed_exponentiiationi_factor = 16,
+      }
+    end
+  },
+
+  -- Statusbar
+  { 'nvim-lualine/lualine.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    event = {
+      'BufRead',
+      'BufNewFile'
+    },
+    config = function()
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = ''},
+          section_separators = { left = '', right = ''},
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+          }
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'filename'},
+          lualine_x = {'encoding', 'fileformat', 'filetype'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {'filename'},
+          lualine_x = {'location'},
+          lualine_y = {},
+          lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+      }
+    end
+  },
+
+  -- Appearance
+  { 'EdenEast/nightfox.nvim',
+    config = 'vim.cmd [[colorscheme duskfox]]' },
+
+  -- Icon
+  { 'nvim-tree/nvim-web-devicons' },
+
+  -- Tab bar
+  { 'romgrk/barbar.nvim',
+    event = {
+      'BufRead',
+      'BufNewFile'
+    },
+    config = function()
+      require('bufferline').setup {
+        icons = true
+      }
+    end
+  },
+
+  -- File explorer
+  { 'lambdalisue/fern.vim',
+    event = {
+      'BufRead',
+      'BufNewFile'
+    }
+  }
+}
+
+-- Handle vim notifications
+vim.notify = require('notify')
+
+-- New tab
+vim.keymap.set('', '<A-t>', ':enew<CR>', { noremap = true, silent = true })
+
+-- Switch tab
+vim.keymap.set('', '<A-,>', ':BufferPrevious<CR>', { noremap = true, silent = true })
+vim.keymap.set('', '<A-.>', ':BufferNext<CR>', { noremap = true, silent = true })
+
+-- Close tab
+vim.keymap.set('', '<A-w>', ':BufferClose<CR>', { noremap = true, silent = true })
+
+-- Kill tab
+vim.keymap.set('', '<A-q>', ':BufferClose!<CR>', { noremap = true, silent = true })
+
+-- Toggle explorer pane
+vim.keymap.set('', '<A-S-f>', ':Fern . -drawer -toggle <CR>', { noremap = true, silent = true })
---
+++ lua/util.lua
@@ -0,0 +1,59 @@
+local util = {}
+
+function util.assign(assignee)
+  return function(obj)
+    for key, value in pairs(obj) do
+      assignee[key] = value
+    end
+  end
+end
+
+function util.in_order(plugins)
+  return function(opts)
+    local result = {}
+    for i, plugin in ipairs(plugins) do
+      if type(plugin) == 'table' then
+        result[i] = plugin
+      else
+        result[i] = { plugin } 
+      end
+      if i ~= 1 then
+        result[i].after = result[i-1][1]:gsub('.+/', '')
+      end
+    end
+    util.assign(result[#plugins])(opts)
+    return result
+  end
+end
+
+function util.augroup(name)
+  return function(specs)
+    local group = vim.api.nvim_create_augroup(name, { clear = true })
+    for event, spec in pairs(specs) do
+      spec.options.group = group
+      vim.api.nvim_create_autocmd(spec.events, spec.options)
+    end
+  end
+end
+
+function util.autocmd(events)
+  return function(options)
+    return { events=events, options=options }
+  end
+end
+
+function util.command(name)
+  return function(command)
+    return function(opts)
+      vim.api.nvim_create_user_command(name, command, opts)
+    end
+  end
+end
+
+function util.alias(name)
+  return function(command)
+    vim.api.nvim_create_user_command(name, command, {})
+  end
+end
+
+return util
