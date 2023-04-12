1a9ffe216728ffc5a0e19ce470e1916aaeead0e9
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2023-04-12 09:29:39 +0900

    Delete Install.bat

--- Install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-mkdir %USERPROFILE%\.config\
-mkdir %USERPROFILE%\.config\nvim
-copy /Y init.lua %USERPROFILE%\.config\nvim\init.lua
-xcopy /Y lua %USERPROFILE%\.config\nvim\lua\*
-@echo Done!
-timeout 2 /NOBREAK > NUL
